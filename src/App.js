b00cdf9a8f2d01ec0e735d9c2a8d2d15b8ec4d28
Chidera <okekechidera97@gmail.com>
2020-12-02 16:56:46 +0100

    updated Main.jsx

--- src/App.js
+++ src/App.js
@@ -4,17 +4,14 @@ import Header from "./components/Header";
 import Main from "./components/Main";
 
 function App() {
-	const [inputs, setInputs] = useState({});
 	const [onMetric, setOnMetric] = useState(true);
 
 	const toggleMetric = (e) => setOnMetric(!onMetric);
 
-	const handleChange = (e) => {};
-
 	return (
 		<div className="container">
 			<Header onMetric={onMetric} toggleMetric={toggleMetric} />
-			<Main handleChange={handleChange} />
+			<Main />
 		</div>
 	);
 }
--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,22 +1,65 @@
-import React from "react";
+import React, { useReducer } from "react";
 
-function Main({ handleChange }) {
+function Main() {
+	/**setting up the hook which handles inputs, best to use reducer as there are multiple inputs */
+	const [userInput, setUserInput] = useReducer(
+		(state, newState) => ({ ...state, ...newState }),
+		{ height: "", weight: "" }
+	);
+	const handleChange = (e) => {
+		const { name, value } = e.target;
+		setUserInput({ [name]: value });
+	};
+
+	/** Initialise BMI and destructure weight and height from the useReducer */
+
+	const { weight, height } = userInput;
+
+	/**Calculate the BMI and round to two decimal places */
+	let BMI = null;
+	const calculateBMI = () => {
+		BMI = weight / (height * height);
+		console.log(BMI);
+		return BMI;
+		//let BMIValue = Math.round(calcBMI * 100) / 100;
+		//return BMIValue;
+	};
+
+	console.log(BMI);
+
+	/**const calculateBMI = () => {
+		const calcBMI = weight / (height * height);
+		let BMIValue = Math.round(calcBMI * 100) / 100;
+		return BMIValue;
+	};
+
+	const BMI = calculateBMI();
+	console.log(BMI);*/
+
 	return (
 		<div className="container">
 			<div className="m-3">
-				<label htmlFor="height">
-					Height:
-					<input type="number" name="height" onChange={handleChange} />
-				</label>
+				<label htmlFor="height">Height:</label>
+				<input
+					type="number"
+					name="height"
+					onChange={handleChange}
+					value={userInput.height}
+				/>
 			</div>
 			<div className="m-3">
-				<label htmlFor="weight">
-					Weight:
-					<input type="number" name="weight" onChange={handleChange} />
-				</label>
+				<label htmlFor="weight">Weight:</label>
+				<input
+					type="number"
+					name="weight"
+					onChange={handleChange}
+					value={userInput.weight}
+				/>
 			</div>
+			<button className="btn btn-outline" onClick={calculateBMI}>
+				Calculate
+			</button>
 		</div>
 	);
 }
-
 export default Main;
